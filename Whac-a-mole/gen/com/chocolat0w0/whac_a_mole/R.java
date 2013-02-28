3811389c7e532e58e95e05a96dc8989bfccfc094
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2013-02-28 17:22:45 +0900

    初期画面表示

--- Whac-a-mole/gen/com/chocolat0w0/whac_a_mole/R.java
+++ Whac-a-mole/gen/com/chocolat0w0/whac_a_mole/R.java
@@ -16,12 +16,17 @@ public final class R {
     public static final class id {
         public static final int btn_close=0x7f070001;
         public static final int btn_restart=0x7f070000;
-        public static final int btn_start=0x7f070002;
-        public static final int menu_settings=0x7f070003;
+        public static final int btn_start=0x7f070006;
+        public static final int menu_settings=0x7f070007;
+        public static final int txt_point=0x7f070005;
+        public static final int txt_point_title=0x7f070003;
+        public static final int txt_time_title=0x7f070002;
+        public static final int txt_timer=0x7f070004;
     }
     public static final class layout {
         public static final int end_menu=0x7f030000;
-        public static final int start_menu=0x7f030001;
+        public static final int game_menu=0x7f030001;
+        public static final int start_menu=0x7f030002;
     }
     public static final class menu {
         public static final int activity_end_menu=0x7f060000;
@@ -34,6 +39,7 @@ public final class R {
         public static final int menu_settings=0x7f040002;
         public static final int point_init=0x7f040007;
         public static final int point_title=0x7f040006;
+        public static final int rest_time=0x7f040008;
         public static final int restart=0x7f040004;
         public static final int start=0x7f040003;
     }
--- Whac-a-mole/src/com/chocolat0w0/whac_a_mole/Game.java
+++ Whac-a-mole/src/com/chocolat0w0/whac_a_mole/Game.java
@@ -3,44 +3,45 @@ package com.chocolat0w0.whac_a_mole;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.RelativeLayout.LayoutParams;
+import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
 public class Game extends Activity {
+	private static final int LIMIT_TIME_SECONDS = 30;
 	private RelativeLayout viewGroup;
-	private TextView txtPointTitle;
-	private TextView txtPoint;
-	private TextView txtTimer;
+	private View gameMenu;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		viewGroup = new RelativeLayout(this);
-		displayBackground();		
-		setContentView(viewGroup);
+		setViews();
 	}
 
-	private void displayBackground() {
+	private void setViews() {
+		viewGroup = new RelativeLayout(this);
+		gameMenu = getLayoutInflater().inflate(R.layout.game_menu, viewGroup, isChild());
 		viewGroup.setBackgroundColor(Color.GREEN);
-		
-		txtTimer = new TextView(this);
-		txtTimer.setText("hello");
-		LayoutParams lpTimer = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		viewGroup.addView(txtTimer, lpTimer);
-		
-		txtPointTitle = new TextView(this);
-		LayoutParams lpPointTitle = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		txtPointTitle.setText(R.string.point_title);
-		txtPointTitle.setId(1);
-		viewGroup.addView(txtPointTitle, lpPointTitle);
-		
-		txtPoint = new TextView(this);
-		LayoutParams lpPoint = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		lpPoint.addRule(RelativeLayout.RIGHT_OF, 1);
-		txtPoint.setText(R.string.point_init);
-		viewGroup.addView(txtPoint, lpPoint);
+		displayInitTime();		
+		viewGroup.addView(gameMenu);
+		setContentView(viewGroup);		
+	}
+
+	private void displayInitTime() {
+		TextView txtTimer = (TextView) gameMenu.findViewById(R.id.txt_timer);
+		String time = secondsToMMSS(LIMIT_TIME_SECONDS);
+		txtTimer.setText(time);
+	}
+
+	private String secondsToMMSS(int timeSeconds) {
+		int minit;
+		int seconds;
+		String result;
+		minit = timeSeconds / 60;
+		seconds = timeSeconds % 60;
+		result = Integer.toString(minit) + ":" + Integer.toString(seconds);
+		return result;
 	}
 	
-
 }
